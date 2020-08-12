27f0d04350b0e0633b89977ef965d2a1bbfbc987
Ankit <ankit.escalate360@gmail.com>
2020-08-12 13:22:27 +0530

    logout button change

--- client/app/components/UserAccount/Account.jsx
+++ client/app/components/UserAccount/Account.jsx
@@ -25,7 +25,12 @@ const Account = ({ loadingState, tokenState }) => {
   return (
     <div>
       <h1>Account</h1>
-      <button onClick={handleLogOut}>Logout</button>
+      <button
+        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+        onClick={handleLogOut}
+      >
+        Logout
+      </button>
     </div>
   );
 };
